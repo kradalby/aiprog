96fa473d3dcc75cfe56e3b2e13791daf90e30b66
Freboto <toernvall@gmail.com>
2015-08-26 16:01:25 +0200

    stuff i have done

--- p1/m1/astar.py
+++ p1/m1/astar.py
@@ -3,16 +3,32 @@ __author__ = 'Fredrik'
 
 class Astar:
 
-    def astar(graph, start, end):
 
-        open = []
-        close = []
+    def generate_path(self, node):
+        path = [node]
+
+        while node.parent is not None:
+            node = node.parent
+            path.insert(0, node)
+
+        return path
+
+    def astar(self, graph, start, end):
+
+        open_list = []
+        close_list = []
 
         start.g = 0
         start.calculate_heuristic()
         start.calculate_f()
-        open.append(start)
+        open_list.append(start)
 
-        while open:
-            current_node = open.pop()
-            
+        while open_list:
+            current_node = open_list.pop()
+            close_list.append(current_node)
+
+            current_path = self.generate_path(current_node)
+            if current_node is end:
+                return current_path
+
+        
