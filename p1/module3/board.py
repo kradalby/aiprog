d408a9eb9bdf2541a400112ad32eb0ebb9e7c755
tornvall <tornvall@stud.ntnu.no>
2015-10-06 08:46:17 +0200

    this does not look good at the moment

--- p1/module3/board.py
+++ p1/module3/board.py
@@ -5,6 +5,7 @@ class Board():
     def __init__(self, filename):
         self.read_file(filename)
 
+
     def read_file(self, filename):
         with open(filename, 'r') as file:
             content = file.read().splitlines()
@@ -15,8 +16,16 @@ class Board():
             #num_rows = int(self.dimensions.pop(0))
             #num_cols = int(self.dimensions.pop(0))
 
-            self.num_rows = int(self.dimensions.pop(0))
             self.num_cols = int(self.dimensions.pop(0))
+            self.num_rows = int(self.dimensions.pop(0))
+
+            self.matrix = []
+
+            for y in range(self.num_rows):
+                self.matrix.append([])
+                for x in range(self.num_cols):
+                    self.matrix[y].append(0)
+
 
             for _ in range(self.num_rows):
                 temp = content.pop(0).split()
@@ -24,7 +33,10 @@ class Board():
                 self.rows.append(list(map(lambda x: int(x), temp)))
                     #(int(temp[0]), int(temp[1])))
 
+
             for _ in range(self.num_cols):
                 temp = content.pop(0).split()
                 print("this is col: ", temp)
                 self.cols.append(list(map(lambda x: int(x), temp)))
+
+            self.rows.reverse()
--- p1/module3/view.py
+++ p1/module3/view.py
@@ -9,99 +9,35 @@ from datastructure.nonograms import CSPState
 from datastructure.nonograms import Constraint, Variable
 
 
-SIZE = 10
+SIZE = 30
 
 
 class Main(Frame):
-    '''
-    Class responsible for drawing the graphical interface, and the steps of the algorithm.
-    '''
-
-
     def __init__(self, parent):
-        '''
-        Initializes tkinter, and the guis controller.
-        '''
-        Frame.__init__(self, parent)
+        Frame.__init__(self, parent, background='white')
+
         self.parent = parent
-        self.width = 600
-        self.height = 600
-        self.canvas = Canvas(self.parent, borderwidth=0, highlightthickness=0, width=self.width, height=self.height)
-        self.tiles = {}
+        self.board = None
+        self.current_file = None
+        self.canvas = None
+        self.view_level = 0
         self.init_ui()
+        self.node_dic = {}
 
-    def build(self):
-        '''
-        Builds a graphial representation of a given board.
-        '''
-        self.canvas.delete('all')
-        #self.board = board
-        print(dir(self.board))
-        #self.rows = self.board.number_of_columns
-        #self.columns = self.board.number_of_rows
-        self.rows = self.board.num_cols
-        self.columns = self.board.num_rows
-        if self.rows > self.columns:
-            self.square_size = (self.width / self.rows) / 1.5
-        else:
-            self.square_size = (self.height / self.columns) / 1.5
-        for row in reversed(range(self.rows)):
-            for column in range(self.columns):
-                x1 = row * self.square_size
-                y1 = column * self.square_size
-                x2 = row * self.square_size + self.square_size
-                y2 = column * self.square_size + self.square_size
-                self.tiles[(row, column)] = self.canvas.create_rectangle(x1, y1, x2, y2)
+    def generate_matrix(self, state):
 
-        draw_start_x = (self.square_size * self.rows) + self.square_size
-        draw_start_y = (self.square_size * self.columns) + self.square_size
-        x_count = 0
-        y_count = self.square_size / 2
-        for row in self.board.rows:
-            for num in row:
-                self.canvas.create_text(draw_start_x + x_count, y_count, text=str(num))
-                x_count += 20
-            y_count += self.square_size
-            x_count = 0
+        matrix = [None for x in range(self.board.num_cols)]
+        for var in state.variables.values():
+            if var.id[0] == 0:
+                matrix[var.id[1]] = var.domain[0]
+        self.board.matrix = matrix
 
-        x_count = self.square_size / 2
-        y_count = 0
-        for column in self.board.cols:
-            for num in column:
-                self.canvas.create_text(x_count, draw_start_y + y_count, text=str(num))
-                y_count += 20
-            y_count = 0
-            x_count += self.square_size
-
-        print(self.canvas)
-        self.canvas.pack()
-        self.run()
-
-    def draw_square(self, x, y, num):
-        '''
-        Draws a square from its x and y coordinates, with the given color.
-        '''
-        x1 = x * self.square_size
-        y1 = y * self.square_size
-        x2 = x1 + self.square_size
-        y2 = y1 + self.square_size
-        if num == 1:
-            self.canvas.create_rectangle(x1, y1, x2, y2, fill='blue')
-        else:
-            self.canvas.create_rectangle(x1, y1, x2, y2, fill='white')
-
-    def color_vertexes(self, state):
-        '''
-        Colors all of the vertexes.
-        '''
-        #for variable in variables:
-        #    if len(variable.domain) == 1 and variable.domain[0] == 0:
-        #        for index in range(len(current.domains[domain][0])):
-        #            self.draw_square(domain[1], index, current.domains[domain][0][index])
-        for variable in state.variables.values():
-            if variable.id[0] == 1:
-                print('ROOW: ',variable.domain)
-
+        for x in matrix:
+            print(x)
+        print('---------------------------------')
+        for y in state.variables.values():
+            if y.id[0] == 1:
+                print(y.domain[0])
 
     def init_ui(self):
 
@@ -117,23 +53,43 @@ class Main(Frame):
 
         self.add_boards_to_menu(boardsmenu)
 
-        #self.canvas = Canvas(self, width=800, height=600)
-        #self.canvas.config(bg='white')
-        #self.canvas.pack(fill=BOTH, expand=1)
+        self.canvas = Canvas(self, width=800, height=600)
+        self.canvas.config(bg='white')
+        self.canvas.pack(fill=BOTH, expand=1)
 
-        #self.pack(fill=BOTH, expand=1)
+        self.pack(fill=BOTH, expand=1)
 
-        #self.optionsmenu = optionsmenu
+        self.optionsmenu = optionsmenu
 
 
     def createmap(self, f=None):
 
         self.current_file = f
         self.board = Board(f)
-        self.build()
-        print(self.tiles)
-        #self.run()
+        self.draw_map()
+        self.run()
 
+    def draw_map(self):
+        self.canvas.delete('all')
+
+        for y in range(len(self.board.matrix) - 1, -1, -1):
+            for x in range(len(self.board.matrix[y])):
+                coords = (
+                    x * SIZE + 3,
+                    #(len(self.board.matrix[y]) - y) * SIZE + 3,
+                    y * SIZE + 3,
+                    x * SIZE + (SIZE + 2),
+                    #(len(self.board.matrix[y]) - y) * SIZE + (SIZE + 2),
+                    y * SIZE + (SIZE + 2),
+                )
+                self.node_dic[(x, y)] = self.canvas.create_rectangle(*coords,
+                                             fill= "white")
+
+    def update_map(self):
+        for y in range(len(self.board.matrix)):
+            for x in range(len(self.board.matrix[y])):
+                if self.board.matrix[y][x]:
+                    self.canvas.itemconfig(self.node_dic[(x, y)], fill='pink')
 
     def add_boards_to_menu(self, menu):
 
@@ -148,21 +104,21 @@ class Main(Frame):
 
         gac = NonogramGAC()
         nodes = {}
-        function = make_function(['x, y'], 'x == y') #x[1][x[0]]
+        function = make_function(['x', 'y'], 'x == y') #x[1][x[0]]
         constraints = {}
 
         for row in range(len(self.board.rows)):
             constraints[(0, row)] = [(1, x) for x in range(len(self.board.cols))]
             node = Variable((0, row))
-            node.domain = generate_permutations(self.board.rows[row], len(self.board.rows))
-            #print("xxxxx", self.board.rows[row])
+            node.domain = generate_permutations(self.board.rows[row], len(self.board.cols))
+            print("xxxxx", self.board.rows[row])
             print(node.domain)
             nodes[(0, row)] = node
 
         for col in range(len(self.board.cols)):
             constraints[(1, col)] = [(0, x) for x in range(len(self.board.rows))]
             node = Variable((1, col))
-            node.domain = generate_permutations(self.board.cols[col], len(self.board.cols))
+            node.domain = generate_permutations(self.board.cols[col], len(self.board.rows))
             nodes[(1, col)] = node
 
 
@@ -179,8 +135,10 @@ class Main(Frame):
         astar_csp.initialize()
 
 
+
         for r in astar_csp.run():
             if r[0]:
                 s = r[0][-1]
-                self.color_vertexes(s)
+                self.generate_matrix(s)
+                self.update_map()
                 print("This is s: ", s)
