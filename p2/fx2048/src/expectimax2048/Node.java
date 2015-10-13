51a52445230672afb4c394a0e5bb8ef43658289a
Kristoffer Dalby <kradalby@kradalby.no>
2015-10-13 09:13:17 +0200

    added the creation of permutations

--- p2/fx2048/src/expectimax2048/Node.java
+++ p2/fx2048/src/expectimax2048/Node.java
@@ -3,21 +3,24 @@ package expectimax2048;
 import game2048.Location;
 import game2048.Tile;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.awt.*;
+import java.util.*;
 
 /**
  * Created by kradalby on 12/10/15.
  */
 public class Node {
     private int[][] board;
+    private Node parent;
+    private ArrayList<Point> empty;
+
     public Node() {
+        empty = new ArrayList<>();
     }
 
     public Node getLeft() {
         Node node = new Node();
+        node.setParent(this);
         node.setBoard(this.getCopyOfBoard());
         node.moveLeft();
         return node;
@@ -25,6 +28,7 @@ public class Node {
 
     public Node getRight() {
         Node node = new Node();
+        node.setParent(this);
         node.setBoard(this.getCopyOfBoard());
         Util.inplaceReverse(node.getBoard());
         node.moveLeft();
@@ -34,6 +38,7 @@ public class Node {
 
     public Node getUp() {
         Node node = new Node();
+        node.setParent(this);
         node.setBoard(this.getCopyOfBoard());
         Util.inplaceRotate(node.getBoard());
         Util.inplaceRotate(node.getBoard());
@@ -45,6 +50,7 @@ public class Node {
 
     public Node getDown() {
         Node node = new Node();
+        node.setParent(this);
         node.setBoard(this.getCopyOfBoard());
         Util.inplaceRotate(node.getBoard());
         node.moveLeft();
@@ -53,6 +59,24 @@ public class Node {
         Util.inplaceRotate(node.getBoard());
 
         return node;
+    }
+
+    public ArrayList<Node> getPermutations() {
+        ArrayList<Node> nodes = new ArrayList<>();
+        for (Point t: empty) {
+            Node node2 = new Node();
+            node2.setParent(this);
+            node2.setBoard(this.getCopyOfBoard());
+            node2.getBoard()[t.y][t.x] = 2;
+            nodes.add(node2);
+
+            Node node4 = new Node();
+            node4.setParent(this);
+            node4.setBoard(this.getCopyOfBoard());
+            node4.getBoard()[t.y][t.x] = 4;
+            nodes.add(node4);
+        }
+        return nodes;
     }
 
     private void moveLeft() {
@@ -73,6 +97,7 @@ public class Node {
                 }
             }
 
+            // Move all the things
             for (int i = 0; i < this.board[row].length; i++) {
                 if (this.board[row][i] == 0 && this.board[row].length > i + 1) {
                     this.board[row][i] = this.board[row][i+1];
@@ -84,6 +109,13 @@ public class Node {
                 if (this.board[row][i] == 0 && this.board[row].length > i + 1) {
                     this.board[row][i] = this.board[row][i+1];
                     this.board[row][i+1] = 0;
+                }
+            }
+
+            // Save all the empty spots
+            for (int i = 0; i < this.board[row].length; i++) {
+                if (this.board[row][i] == 0) {
+                    this.empty.add(new Point(i, row));
                 }
             }
         }
@@ -91,10 +123,9 @@ public class Node {
 
     public void populateBoard(Map<Location, Tile> grid) {
         System.out.println(new PrettyPrintingMap<Location, Tile>(grid));
-
     }
 
-    public int[][] getCopyOfBoard() {
+    private int[][] getCopyOfBoard() {
         int[][] newBoard = new int[4][4];
         for (int i = 0; i < this.board.length; i++) {
             newBoard[i] = this.board[i].clone();
@@ -106,20 +137,17 @@ public class Node {
         return this.board;
     }
 
-    public void setBoard(int[][] board) {
-        this.board = board;
+
+    public Node getParent() {
+        return parent;
     }
 
-    public static void main(String[] args) {
-        Node node = new Node();
-        node.setBoard(new int [][] {
-                {2,2,0,2},
-                {2,2,2,2},
-                {0,2,2,4},
-                {0,0,4,2}
-        });
-        node = node.getRight();
-        System.out.println(node);
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public void setBoard(int[][] board) {
+        this.board = board;
     }
 
     @Override
@@ -133,6 +161,23 @@ public class Node {
             s += "\n";
         }
         return s;
+    }
+
+    public static void main(String[] args) {
+        Node node = new Node();
+        node.setBoard(new int [][] {
+                {2,2,0,2},
+                {2,4,2,2},
+                {0,2,2,4},
+                {0,0,4,2}
+        });
+        node = node.getDown();
+        System.out.println("stuff");
+        ArrayList<Node> nodes = node.getPermutations();
+        System.out.println("derp");
+        for (Node n : nodes) {
+            System.out.println(n);
+        }
     }
 }
 
