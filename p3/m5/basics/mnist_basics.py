cfaa4a906339db719237037266063585c0589eec
Kristoffer Dalby <kradalby@kradalby.no>
2015-11-15 14:23:04 +0100

    made path relativ and commented out motplat

--- p3/m5/basics/mnist_basics.py
+++ p3/m5/basics/mnist_basics.py
@@ -6,7 +6,7 @@ __author__ = 'keithd'
 import os, struct
 import time
 from array import array as pyarray
-import matplotlib.pyplot as pyplot
+#import matplotlib.pyplot as pyplot
 import numpy
 import pickle
 
@@ -19,7 +19,7 @@ def kd_reduce(func,seq):
 
 # Set this to the complete path to your mnist files.
 ## __mnist_path__ = "path/to/all/your/mnist/files"
-__mnist_path__ = "/Users/keithd/core/python/data/mnist/basics/"
+__mnist_path__ = os.path.dirname(os.path.abspath(__file__))
 
 # The load_mnist function is the main interface between the MNIST files and your machine-learning code.  It fetches
 # subsets of the entire training or test sets, as determined by the 'digits'
