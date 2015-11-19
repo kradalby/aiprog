7682efbca5fb9600769969c9cf0303bbf53f5689
Kristoffer Dalby <kradalby@kradalby.no>
2015-11-19 08:48:18 +0100

    2048

---
+++ p3/m6/2048.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+''' Help the user achieve a high score in a real game of 2048 by using a move searcher. '''
+
+from __future__ import print_function
+import ctypes
+import time
+import os
+from ann import ANN
+from dump import *
+
+ann = ANN([16, 8, 8, 8, 4], ['rect', 'rect', 'rect', 'soft'])
+
+def to_c_board(m):
+    board = 0
+    i = 0
+    for row in m:
+        for c in row:            
+            board |= c << (4*i)
+            i += 1
+    return board
+
+def print_board(m):
+    for row in m:
+        for c in row:
+            print('%8d' % c, end=' ')
+        print()
+
+def _to_val(c):
+    if c == 0: return 0
+    return 2**c
+
+def to_val(m):
+    return [[_to_val(c) for c in row] for row in m]
+
+def _to_score(c):
+    if c <= 1:
+        return 0
+    return (c-1) * (2**c)
+
+def to_score(m):
+    return [[_to_score(c) for c in row] for row in m]
+
+def find_best_move(m):
+    move = ann.go(m)
+    move = move[0]
+    print(move)
+    for i in sorted(move)[::-1]:
+        print(i)
+        print(move.tolist().index(i))
+        legal = valid_move(move.tolist().index(i), m)
+        print(legal)
+        if legal:
+            return move.tolist().index(i)
+    return 0
+
+def get_column(i, m):
+    l = []
+    for row in m:
+        l.append(row[i])
+    return l
+
+def valid_move(dir, m):
+    """
+    Test if a move in the given direction is possible
+    U: 0, R: 1: D: 2: L: 3
+    :param dir: Direction to check
+    :param m: The board to check on
+    :return: A boolean telling if a move is valid or not
+    """
+    print(m)
+    size = range(0, 4)
+    if dir == 2 or dir == 3:
+        for x in size:
+            col = m[x]
+            print(col)
+            for y in size:
+                if y < 4 - 1 and col[y] == col[y + 1] and col[y] != 0:
+                    return True
+                if dir == 3 and y > 0 and col[y] == 0 and col[y - 1] != 0:
+                    return True
+                if dir == 2 and y < 4 - 1 and col[y] == 0 and col[y + 1] != 0:
+                    return True
+
+    if dir == 0 or dir == 1:
+        for y in size:
+            line = get_column(y, m)
+            print(line)
+            for x in size:
+                if x < 4 - 1 and line[x] == line[x + 1] and line[x] != 0:
+                    return True
+                if dir == 1 and x > 0 and line[x] == 0 and line[x - 1] != 0:
+                    return True
+                if dir == 0 and x < 4 - 1 and line[x] == 0 and line[x + 1] != 0:
+                    return True
+    return False
+
+def movename(move):
+    return ['up', 'down', 'left', 'right'][move]
+
+def play_game(gamectrl):
+    moveno = 0
+    start = time.time()
+    while 1:
+        state = gamectrl.get_status()
+        if state == 'ended':
+            break
+        elif state == 'won':
+            time.sleep(0.75)
+            gamectrl.continue_game()
+
+        moveno += 1
+        board = gamectrl.get_board()
+        move = find_best_move(board)
+        if move < 0:
+            break
+        print("%010.6f: Score %d, Move %d: %s" % (time.time() - start, gamectrl.get_score(), moveno, movename(move)))
+        gamectrl.execute_move(move)
+
+    score = gamectrl.get_score()
+    board = gamectrl.get_board()
+    maxval = max(max(row) for row in to_val(board))
+    print("Game over. Final score %d; highest tile %d." % (score, maxval))
+
+def parse_args(argv):
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Use the AI to play 2048 via browser control")
+    parser.add_argument('-p', '--port', help="Port number to control on (default: 32000 for Firefox, 9222 for Chrome)", type=int)
+    parser.add_argument('-b', '--browser', help="Browser you're using. Only Firefox with the Remote Control extension, and Chrome with remote debugging, are supported right now.", default='firefox', choices=('firefox', 'chrome'))
+    parser.add_argument('-k', '--ctrlmode', help="Control mode to use. If the browser control doesn't seem to work, try changing this.", default='hybrid', choices=('keyboard', 'fast', 'hybrid'))
+
+    return parser.parse_args(argv)
+
+def main(argv):
+    args = parse_args(argv)
+
+    if args.browser == 'firefox':
+        from ffctrl import FirefoxRemoteControl
+        if args.port is None:
+            args.port = 32000
+        ctrl = FirefoxRemoteControl(args.port)
+    elif args.browser == 'chrome':
+        from chromectrl import ChromeDebuggerControl
+        if args.port is None:
+            args.port = 9222
+        ctrl = ChromeDebuggerControl(args.port)
+
+    if args.ctrlmode == 'keyboard':
+        from gamectrl import Keyboard2048Control
+        gamectrl = Keyboard2048Control(ctrl)
+    elif args.ctrlmode == 'fast':
+        from gamectrl import Fast2048Control
+        gamectrl = Fast2048Control(ctrl)
+    elif args.ctrlmode == 'hybrid':
+        from gamectrl import Hybrid2048Control
+        gamectrl = Hybrid2048Control(ctrl)
+
+    if gamectrl.get_status() == 'ended':
+        gamectrl.restart_game()
+
+    play_game(gamectrl)
+
+if __name__ == '__main__':
+    import sys
+    exit(main(sys.argv[1:]))
---
+++ p3/m6/ann.py
@@ -0,0 +1,181 @@
+import theano
+from theano import tensor as T
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+import numpy as np
+from dump import *
+
+srng = RandomStreams()
+
+class Layer:
+    def __init__(self, input, output, activation):
+        self.input = input
+        self.output = output
+        self.activation_function = activation
+
+    @staticmethod
+    def floatX(X):
+        return np.asarray(X, dtype=theano.config.floatX)
+
+    def create_shape(self):
+        self.shape = theano.shared(self.floatX(np.random.randn(self.input, self.output) * 0.01))
+
+class ANN:
+    def __init__(self, layer_sizes, layer_activations):
+        self.boards, self.moves = load_all('dump.pkl')
+
+        self.X = T.fmatrix()
+        self.Y = T.fmatrix()
+
+        self.create_layers(layer_sizes, layer_activations)
+
+        self.pyx = self.model(self.X)
+
+        self.yx = T.argmax(self.pyx, axis=1)
+
+        # SOMETHING CAN BE WRONG HERE
+        self.output = self.layers[-1].shape
+        self.cost = T.sum((self.Y-self.pyx)**2, acc_dtype=theano.config.floatX)
+        # cost = T.mean(T.nnet.categorical_crossentropy(X, Y))
+        self.params = [x.shape for x in self.layers]
+        self.updates = self.RMSprop(self.cost, self.params, lr=0.001)
+
+        self.train = theano.function(inputs=[self.X, self.Y], outputs=self.cost, updates=self.updates, allow_input_downcast=True)
+        self.predict = theano.function(inputs=[self.X], outputs=self.pyx, allow_input_downcast=True)
+
+        tri = self.boards
+        trl = self.moves
+        for i in range(30):
+            for start, end in zip(list(range(0, len(tri), 128)), list(range(128, len(tri), 128))):
+                self.cost = self.train(tri[start:end], trl[start:end])
+            #print(np.mean(np.argmax(ann.test_labels, axis=1) == ann.predict(ann.test_images)))
+            #print(ann.predict(ann.test_images))
+
+
+    def convert_number_to_array(self, data):
+        converted_data = []
+        for label in data:
+            arr = np.zeros(10, dtype=theano.config.floatX)
+            print(label)
+            arr.put(np.float32(label), np.float32(1))
+            converted_data.append(arr)
+
+        return converted_data
+
+
+    # Activation function
+    @staticmethod
+    def rectify(flatten_input_matrix):
+        return T.maximum(flatten_input_matrix, 0.)
+
+    @staticmethod
+    def softmax(flatten_input_matrix):
+        e_x = T.exp(flatten_input_matrix - flatten_input_matrix.max(axis=1).dimshuffle(0, 'x'))
+        return e_x / e_x.sum(axis=1).dimshuffle(0, 'x')
+
+    @staticmethod
+    def sigmoid(flatten_input_matrix):
+        return T.nnet.sigmoid(flatten_input_matrix)
+
+    @staticmethod
+    def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
+        grads = T.grad(cost=cost, wrt=params)
+        updates = []
+        for p, g in zip(params, grads):
+            acc = theano.shared(p.get_value() * 0.)
+            acc_new = rho * acc + (1 - rho) * g ** 2
+            gradient_scaling = T.sqrt(acc_new + epsilon)
+            g = g / gradient_scaling
+            updates.append((acc, acc_new))
+            updates.append((p, p - lr * g))
+        return updates
+
+
+    def create_layers(self, layer_sizes, layer_activations):
+        '''
+        Note:
+            Both lists must be the same length.
+q
+        Args:
+            layer_sizes: List of sizes for the layers, including input and output.
+                Format: [10, 20, 30, 40]
+            layer_activations: List of activation functions for each layer.
+                Format: ["rect", "soft", "sig"]
+
+        Attributes:
+            layers:
+        '''
+
+        assert len(layer_sizes)-1 == len(layer_activations)
+
+        self.layers = []
+        input = layer_sizes.pop(0)
+
+        for size, activation in zip(layer_sizes, layer_activations):
+            layer = None
+            if activation == 'rect':
+                layer = Layer(input, size, self.rectify)
+            elif activation == 'soft':
+                layer = Layer(input, size, self.softmax)
+            elif activation == 'sig':
+                layer = Layer(input, size, self.sigmoid)
+
+            print('created layer with: ({}, {}), {}'.format(input, size, activation))
+            input = size
+            layer.create_shape()
+            self.layers.append(layer)
+
+    def model(self, X):
+        #l = self.noise_removal(X, drop_input)
+
+        l = X
+        for layer in self.layers:
+            l = layer.activation_function(T.dot(l, layer.shape))
+
+        return l
+
+
+    def go(self, board=None):
+        if board:
+            board = np.asarray(board, dtype=theano.config.floatX).flatten()
+            return self.predict([board])
+
+        predictions = self.predict(self.boards)
+        print(len(predictions))
+        #return predictions.tolist()
+        print(predictions)
+        print(np.mean(np.argmax(self.moves, axis=1) == predictions))
+
+
+
+if __name__ == "__main__":
+
+
+    scenario_sizes = [
+        [16, 8, 4],
+    ]
+
+    scenario_act = [
+        ['rect', 'soft'],
+    ]
+
+    ann = ANN(scenario_sizes[0], scenario_act[0])
+    ann.go()
+
+    #minor_demo(ann)
+
+    #for sizes, activations in zip(scenario_sizes, scenario_act):
+    #    ann = ANN(sizes, activations)
+
+
+    #    tri = ann.training_images
+    #    trl = ann.training_labels
+
+    #    ann.test_images, ann.test_labels = load_flat_cases_as_sets('demo_prep')
+    #    ann.grayscale()
+    #    ann.test_labels = ann.convert_number_to_array(ann.test_labels)
+
+    #    for i in range(20):
+    #        for start, end in zip(list(range(0, len(tri), 128)), list(range(128, len(tri), 128))):
+    #            ann.cost = ann.train(tri[start:end], trl[start:end])
+    #        print(np.mean(np.argmax(ann.test_labels, axis=1) == ann.predict(ann.test_images)))
+    #        print(ann.predict(ann.test_images))
